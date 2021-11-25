9f7fbcda401b5f097077658c534abdcd617effd9
fahmi <fahminur.dev@gmail.com>
2021-11-25 21:30:13 +0700

    added mqtt service; added device code generator

--- services/generator.js
+++ services/generator.js
@@ -1,7 +1,13 @@
 const crypto = require('crypto')
 
 const randomString = (bytes) => crypto.randomBytes(bytes).toString('hex')
+const deviceCode = (data, id) => {
+  const str = data[id].split('/')
+
+  return str[2]
+}
 
 module.exports = {
-  randomString
+  randomString,
+  deviceCode
 }
